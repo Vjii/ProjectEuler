18f88db21dcc360cd57c62da2cb98300825f5321
Jan Pilik <snyshuem@gmail.com>
2017-01-12 21:21:17 +0100

    Fix variable in the first challenge to properly include the user specified number in the sum of multiples

--- js/euler1.js
+++ js/euler1.js
@@ -1,11 +1,11 @@
 
-	
+
 	function addMultiples() {
-		var input = document.getElementById("userInput"); 
+		var input = document.getElementById("userInput");
 	   	sum = 0
-			for (i = 0; i < input.value; i++) { 
+			for (i = 0; i <= input.value; i++) {
 				if (i % 3 === 0) {
-					sum += i; 
+					sum += i;
 				} else if (i % 5 === 0) {
 					sum += i;
 				}
@@ -21,7 +21,6 @@
 //1. Add two previous terms to make the new number
 //2. Add the new number to the sequence (variable)
 //3. Check whether the number is even-valued; if so - add it to sum (another variable);
-  	
 
 
 
@@ -33,6 +32,7 @@
 
 
 
-	
 
-	
+
+
+
